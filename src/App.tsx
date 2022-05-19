a4ed526f5b4b340fc4647361fc2a25fcc8c7512a
Christian Possidonio <christian.possidonio@itsector.pt>
2022-05-19 18:14:29 -0300

    redux toolkit ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {useAppSelector, useAppDispatch} from './app/hooks'
+import { incremented, decremented } from './features/counter/counter.slice';
 
 function App() {
+
+  const count = useAppSelector(state => state.counter.value)
+  const dispatch = useAppDispatch()
+
+  const handleClickAdded = () => dispatch(incremented())
+  const handleClickRemoved = () => dispatch(decremented())
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h2>Parceiros</h2>
+       <div style={{display: 'flex'}}>
+         <div style={{padding: '1rem'}}>Parceiro {count}</div>
+         <div style={{padding: '0.2rem '}}><button  onClick={handleClickAdded}>+</button></div>
+         <div style={{padding: '0.2rem '}}>{count === 0 ? <></> : <button  onClick={handleClickRemoved}>-</button>}</div>
+       </div>
       </header>
     </div>
   );
-}
 
+}
 export default App;
---
+++ src/features/counter/counter.slice.ts
@@ -0,0 +1,26 @@
+// DUCKS pattern
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface CounterState {
+  value: number;
+}
+
+const initialState: CounterState = {
+  value: 0,
+};
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    incremented(state) {
+      state.value++;
+    },
+    decremented(state) {
+      state.value--
+    },
+  },
+});
+
+export const { incremented, decremented } = counterSlice.actions;
+export default counterSlice.reducer;
